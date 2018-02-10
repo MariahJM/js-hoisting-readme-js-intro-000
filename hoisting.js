29e4961643d6f7c3e6955ad6afab2b8de238de3a
Mariah Morton <mariahjacobsmorton@gmail.com>
2018-02-10 18:37:12 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -16,13 +16,15 @@ function crazy() {
   thisIsCrazy();
 
 }
-  var name = "Cricky";
+
 function sayMyName() {
   // fix the code in here:
+  var name = "Cricky";
+
+  sayMy();
+
   function sayMy() {
     console.log(name);
     var name = "Kristin";
   }
-  sayMy();
-
 }
